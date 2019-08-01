b7187e42f336763b25348ae6548a3a910ce02509
Andrew Ziulkovskiy <sovieteagle2017@gmail.com>
2019-08-01 16:50:39 +0300

    First change

---
+++ src/main/java/net/proselyte/bookmanager/dao/BookDao.java
@@ -0,0 +1,17 @@
+package net.proselyte.bookmanager.dao;
+
+import net.proselyte.bookmanager.model.Book;
+
+import java.util.List;
+
+public interface BookDao {
+    public void addBook(Book book);
+
+    public void updateBook(Book book);
+
+    public void removeBook(int id);
+
+    public Book getBookById(int id);
+
+    public List<Book> listBooks();
+}
---
+++ src/main/java/net/proselyte/bookmanager/service/BookService.java
@@ -0,0 +1,17 @@
+package net.proselyte.bookmanager.service;
+
+import net.proselyte.bookmanager.model.Book;
+
+import java.util.List;
+
+public interface BookService {
+    public void addBook(Book book);
+
+    public void updateBook(Book book);
+
+    public void removeBook(int id);
+
+    public Book getBookById(int id);
+
+    public List<Book> listBooks();
+}
